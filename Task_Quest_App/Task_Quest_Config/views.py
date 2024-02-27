c8e510a55ee2a27fa3e6f4bc9804d35bb44605b7
Justice Sparks <sparksjustice664@gmail.com>
2024-02-27 15:45:33 +0000

    Testing_new_branch

--- Task_Quest_App/Task_Quest_Config/views.py
+++ Task_Quest_App/Task_Quest_Config/views.py
@@ -4,22 +4,29 @@ Views for Pages of the TaskQuest Application
 
 from django.http import HttpResponse
 from django.views import View, generic
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+from datetime import datetime
+from calendar import HTMLCalendar
 from .models import Task
 
 
 # This will be REPLACED with the view of our home-page 
 def index(request):
-  return HttpResponse("This will be the home page...hopefully soon....?")
+  cal = HTMLCalendar()
+  html_cal = cal.formatmonth(datetime.today().year, datetime.today().month, withyear=True)
+  calendar = mark_safe(html_cal)
+  current_month_year = datetime.today().strftime('%B %Y')
+  return render(request, 'Task_Quest_Config/index.html', {'calendar' : calendar, 'current_month_year': current_month_year})
 
-
-# We need to create a view of the calendar 
 
 '''This page doesn't work with the built-in webview since it uses an anonymous user. 
   Instead, use a new tab logged in as an admin user.'''
 class TaskListView(generic.ListView):
   model = Task
-  def get_context_data(self, **kwargs): #def get_queryset(self
+  def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['task_list'] = Task.objects.filter(user=self.request.user)
     context['current_user'] = self.request.user.username
     return context
+
