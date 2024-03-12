60c11816fed24b0a0f4e197d5cb03e6f89c39622
Justice Sparks <10260450-JusticeSparks@users.noreply.replit.com>
2024-03-12 13:41:39 +0000

    initial custom tasks functionality

--- Task_Quest_App/Task_Quest_Config/models.py
+++ Task_Quest_App/Task_Quest_Config/models.py
@@ -14,6 +14,7 @@ class Task(models.Model):
   time = models.TimeField()
   name = models.CharField(max_length=100)
   difficulty = models.IntegerField()
+  priority = models.IntegerField(default=0)
   email = models.EmailField()
   password = models.CharField(max_length=100)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
--- Task_Quest_App/Task_Quest_Config/views.py
+++ Task_Quest_App/Task_Quest_Config/views.py
@@ -4,19 +4,38 @@ Views for Pages of the TaskQuest Application
 
 from django.http import HttpResponse
 from django.views import View, generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 from datetime import datetime
 from calendar import HTMLCalendar
 from .models import Task
+from .forms import *
 
 
-# This will be REPLACED with the view of our home-page 
+ 
 def index(request):
+
+  '''
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+    if form.is_valid():
+      task = form.save(commit=False)
+      task.user= request.user
+      task.save()
+      return redirect('index')
+
+  else:
+    form = TaskForm()
+  '''
+  # Create an instance of HTMLCalendar
   cal = HTMLCalendar()
-  html_cal = cal.formatmonth(datetime.today().year, datetime.today().month, withyear=True)
+  # Generate HTML for the current month's calendar
+  html_cal = cal.formatmonth(datetime.today().year, datetime.today().month,     withyear=True)
+  # Mark the HTML as safe to prevent autoescaping
   calendar = mark_safe(html_cal)
+  # Get the current month and year in a human-readable format
   current_month_year = datetime.today().strftime('%B %Y')
+  # Render the template with the calendar and current month/year as context  variables
   return render(request, 'Task_Quest_Config/index.html', {'calendar' : calendar, 'current_month_year': current_month_year})
 
 
@@ -30,3 +49,20 @@ class TaskListView(generic.ListView):
     context['current_user'] = self.request.user.username
     return context
 
+def create_task(request):
+  form = TaskForm
+
+  if request.method == 'POST':
+    # Create a new dictionary with form data and movie_title
+    task_data = request.POST.copy()
+    form = TaskForm(task_data, request.FILES)
+    if form.is_valid():
+      # Save the form without committing to the database
+      task = form.save(commit=False)
+      # Set the user relationship
+      task.user = request.user
+      task.save()
+      # Redirect back to the index page
+      return redirect('task-list')
+  context = {'form': form}
+  return render(request, 'Task_Quest_Config/task_form.html', context)
