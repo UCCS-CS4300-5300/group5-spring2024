396916a1205c1da384b29e17f46f72a6bbbbf3f4
Justice Sparks <sparksjustice664@gmail.com>
2024-03-29 16:06:43 +0000

    Made Prev and Next buttons interactable

--- Task_Quest_App/Task_Quest_Config/urls.py
+++ Task_Quest_App/Task_Quest_Config/urls.py
@@ -8,6 +8,10 @@ from . import views
 urlpatterns = [
    
   path('', views.calendar, name='calendar'),
+  path('calendar/<int:year>/<int:month>/', views.calendar, name='calendar'),
+  path('prev/<int:year>/<int:month>/', views.prev_month_view, name='prev_month'),
+  path('next/<int:year>/<int:month>/', views.next_month_view, name='next_month'),
+  
   path('tasks/', views.TaskListView.as_view(), name='task-list'),
   path('addtask/', views.create_task, name='add-task')
 
