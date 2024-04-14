3472b0fb63b566a5a6d9d7b1cf76356daab5cafd
Roodcube <115663992+Roodcube@users.noreply.github.com>
2024-04-14 17:06:13 +0000

    Added a time system to game

--- Task_Quest_App/static/js/script.js
+++ Task_Quest_App/static/js/script.js
@@ -19,13 +19,11 @@ window.addEventListener('load', function(){
         } else if (e.key === ' '){
           this.game.player.shootTop();
         }
-        console.log(this.game.keys);
       });
       window.addEventListener('keyup', e => {
         if (this.game.keys.indexOf(e.key) > -1){  //If an arrow key is released
           this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
         }
-        console.log(this.game.keys);
       });
     }
   }
@@ -89,59 +87,77 @@ window.addEventListener('load', function(){
       });
     }
     shootTop(){
-      this.projectiles.push(new Projectile(this.game, this.x, this.y));
+      if(this.game.ammo > 0){
+        this.projectiles.push(new Projectile(this.game, this.x, this.y));
+        this.game.ammo--;
+      }
     }
-    /*
-    constructor(game) {
-      this.game = game;
-      this.width = 100;
-      this.height = 100;
-      this.x = canvas.width / 2;
-      this.y = canvas.height / 2;
-      this.speed = 5;
-      this.angle = 0;
-      this.frameX = 0;
-      this.frameY = 0;
-      this.frame = 0;
-      this.spriteWidth = 498;
-      this.spriteHeight = 327;
-      this.lives = 3;
-      this.cooldown = 0;
-      this.frames = [
-        { spriteX: 0, spriteY: 0 },
-        { spriteX: 498, spriteY: 0 },
-        { spriteX: 0, spriteY: 327 },
-        { spriteX: 498, spriteY: 327 },
-      ];
-    }*/
   }
   class Enemy {}
   class Layer {}
   class Background {}
-  class UI {}
+  class UI {
+    /*constructor(game){
+      this.game = game;
+      this.fontSize = 25;
+      this.fontFamily = 'Helvetica';
+      this.color = 'white';
+      //this.textAlign = 'left';
+      //this.textBaseline = 'top';
+      //this.score = 0;
+      //this.ammo = 10;
+    }
+    draw(context){
+      //context.font = this.fontSize + 'px ' + this.fontFamily;
+      context.fillStyle = this.color;
+      for (let 1 = 0; i < this.game.ammo; i++){
+        context.fillRect(20 + 5 * i, 50, 3, 20);
+      }
+      //context.textAlign = this.textAlign;
+      //context.textBaseline = this.textBaseline;
+      //context.fillText('Score: ' + this.score, 10, 10);
+      //context.fillText('Ammo: ' + this.ammo, 10, 40);
+    }
+  }*/
   class Game {
     constructor(width, height){
       this.width = width;
       this.height = height;
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      //this.ui = new UI(this);
       this.keys = [];
+      this.ammo = 20;
+      this.maxAmmo = 50;
+      this.ammoTimer = 0;
+      this.ammoInterval = 500;
     }
     update(){
       this.player.update();
+      /*if (this.ammoTimer > this.ammoInterval){
+        if (this.ammo < this.maxAmmo){
+          this.ammo++;
+        }
+        this.ammoTimer = 0;
+      } else {
+        this.ammoTime += deltaTime;
+      }*/
     }
     draw(context){
       this.player.draw(context);
+      //this.ui.draw(context);
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-
-  function animate(){
+  let lastTime = 0;
+  function animate(timeStamp){
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.update();
     game.draw(ctx);
     requestAnimationFrame(animate);
   }
-  animate();
+  animate(0);
 });
