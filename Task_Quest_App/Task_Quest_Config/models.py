87cfb61e766231d579c5f61ce8e4c216422b5509
Justice Sparks <10260450-JusticeSparks@users.noreply.replit.com>
2024-04-15 15:32:02 +0000

    dont know

--- Task_Quest_App/Task_Quest_Config/models.py
+++ Task_Quest_App/Task_Quest_Config/models.py
@@ -1,34 +1,31 @@
-'''
-Data Models for TaskQuest Application 
-'''
+# models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# We need a model for tasks: Date/Time/Name/Difficulty/Description/Status
-
-# We need a model for users: Username/Email/Password
-#Django has a built-in user model so we don't need to create our own
-
 class Task(models.Model):
-  date = models.DateField()
-  time = models.TimeField()
-  name = models.CharField(max_length=100)
-  difficulty = models.IntegerField()
-  priority = models.IntegerField(default=0)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  points = models.IntegerField(default=0)
+    date = models.DateField()
+    time = models.TimeField()
+    name = models.CharField(max_length=100)
+    difficulty = models.IntegerField()
+    priority = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def tasks_for_date(cls, date):
+        return cls.objects.filter(date=date)
 
 class Profile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  total_points = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_points = models.IntegerField(default=0)
 
-  def __str__(self):
-    return self.user.username
+    def __str__(self):
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
--- Task_Quest_App/Task_Quest_Config/urls.py
+++ Task_Quest_App/Task_Quest_Config/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for Task_Quest project.
 """
@@ -8,20 +9,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   
+
   # Home
   path('', views.home_page, name='home'),
-  
+
   # Varying Calendar Views
   path('calendar/', views.calendar, name='calendar'),  
   path('calendar/<int:year>/<int:month>/', views.calendar, name='calendar'),
   path('prev/<int:year>/<int:month>/', views.prev_month_view, name='prev_month'),
   path('next/<int:year>/<int:month>/', views.next_month_view, name='next_month'),
-  
+
   # Task List and Add Task
   path('tasks/', views.TaskListView.as_view(), name='task-list'),
   path('addtask/', views.create_task, name='add-task'),
-  
+
   # Game page 
   path('game/', views.start_game, name='quest-game'),
   path('complete-task/<int:task_id>/', views.complete_task, name='complete_task'),
